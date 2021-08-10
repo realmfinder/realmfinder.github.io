1fd29d23d28db2c8bc29153c8e86897973d4a185
realmfinder <88750762+realmfinder@users.noreply.github.com>
2021-08-11 01:41:34 +0200

    added minimum values for rems and players

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ function submitRequirements() {
 
     document.getElementById("realms").innerHTML = '';
     
-    rems = parseInt(document.getElementById("rems").value);
-    players = parseInt(document.getElementById("players").value);
+    max_rems = parseInt(document.getElementById("max-rems").value);
+    max_players = parseInt(document.getElementById("max-players").value);
+
+    min_rems = parseInt(document.getElementById("min-rems").value);
+    min_players = parseInt(document.getElementById("min-players").value);
 
     let used_uuids = [];
     
@@ -21,7 +24,7 @@ function submitRequirements() {
             let realm_players = parseInt(parts[3]);
             let realm_rems = parseInt(parts[4]);
             let realm_uuid = realm_server+'||'+realm_realm;
-            if (realm_rems < rems && realm_players < players && realm_rems != 0 && !used_uuids.includes(realm_uuid)) {
+            if (realm_rems <= max_rems && realm_rems >= min_rems && realm_players <= max_players && realm_players >= min_players && realm_rems != 0 && !used_uuids.includes(realm_uuid)) {
                 used_uuids.push(realm_uuid)
                 let tag = document.createElement("p");
                 let text = document.createTextNode(`${realm_server} ${realm_realm}: ${realm_players}/85, ${realm_rems} rems.`);
